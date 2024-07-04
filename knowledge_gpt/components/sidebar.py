ee574649311600d77bd713a6c0b227de8d4971df
amcfarlin1991@gmail.com <amcfarlin1991@gmail.com>
2024-07-04 14:56:39 -0400

    Update sidebar.py
    Added more information about NIST and the document

--- knowledge_gpt/components/sidebar.py
+++ knowledge_gpt/components/sidebar.py
@@ -29,15 +29,26 @@ def sidebar():
         st.markdown("---")
         st.markdown("# About")
         st.markdown(
-            "📖KnowledgeGPT allows you to ask questions about your "
-            "documents and get accurate answers with instant citations. "
+            "📖NIST AI 600-1 Helper allows you to ask questions about the "
+            "Artificial Intelligence Risk Management Framework: Generative Artificial Intelligence Profile and get accurate answers with instant citations. "
+			"The most recent version of the document can be found here.[Source](https://www.nist.gov/itl/ai-risk-management-framework)"
+   )
+        st.markdown("# What is the purpose of the Artificial Intelligence Risk Management Framework document?")
+        st.markdown(
+            "In collaboration with the private and public sectors, "
+            "NIST has developed a framework to better manage risks to individuals, organizations, and society" 
+            "associated with artificial intelligence (AI). The NIST AI Risk Management Framework (AI RMF) is intended"
+            " for voluntary use and to improve the ability to incorporate trustworthiness considerations into the design," 
+            "development, use, and evaluation of AI products, services, and systems.[Source](https://www.nist.gov/itl/ai-risk-management-framework)"
         )
         st.markdown(
             "This tool is a work in progress. "
-            "You can contribute to the project on [GitHub](https://github.com/mmz-001/knowledge_gpt) "  # noqa: E501
+            "My fork of the project is located on [GitHub](https://github.com/oohtmeel1/knowledge_gpt) "  # noqa: E501
+            "You can contribute to the main project on [GitHub](https://github.com/mmz-001/knowledge_gpt) " 
             "with your feedback and suggestions💡"
         )
-        st.markdown("Made by [mmz_001](https://twitter.com/mm_sasmitha)")
+        st.markdown("Changes Made by [Oohtmeel1](https://oohtmeel1.github.io/)")
+        st.markdown("Gotta give credit to - Originally Made by [mmz_001](https://twitter.com/mm_sasmitha)")
         st.markdown("---")
 
         faq()
