b624513b4c14edc63578e8bff1b7c6b57a730dfc
amcfarlin1991@gmail.com <amcfarlin1991@gmail.com>
2024-07-08 16:54:31 -0400

    Update ui.py

--- knowledge_gpt/ui.py
+++ knowledge_gpt/ui.py
@@ -35,8 +35,8 @@ The model has line numbers on the left hand side. Please provide those as refere
 {Example - A PDF of a resume is uploaded}
 {Appropriate response - "That document is not one I can talk to you about"}
 
-**IMPORTANT:**
-- Do not describe or discuss the contents of an incorrect document.
+
+- Do not describe or discuss the contents of an incorrect document. Simply say the document is wrong. Simple as that. 
 
 '''
 
