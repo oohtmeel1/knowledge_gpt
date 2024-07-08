e0dea4b1d72e7d241b86f662320b91e5b15f1ec1
amcfarlin1991@gmail.com <amcfarlin1991@gmail.com>
2024-07-08 16:15:41 -0400

    Update main.py

--- knowledge_gpt/main.py
+++ knowledge_gpt/main.py
@@ -21,7 +21,7 @@ from knowledge_gpt.core.utils import get_llm
 
 EMBEDDING = "openai"
 VECTOR_STORE = "faiss"
-MODEL_LIST = ["gpt-4o","GPT-3.5 Turbo"]
+MODEL_LIST = ["gpt-4o","gpt-3.5-turbo-0125"]
 
 # Uncomment to enable debug mode
 # MODEL_LIST.insert(0, "debug")
